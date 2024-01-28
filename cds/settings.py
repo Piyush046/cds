643121248c4f108baad4e748a29ef50a7ea35914
Piyush046 <piyushpandey046@gmail.com>
2024-01-29 04:12:04 +0530

    v10

--- cds/settings.py
+++ cds/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['templates','static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
